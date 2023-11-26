89db8ddbeb931ccc4639cb5aef2c8cc69d4c405c
DanielVarjaskéri <varjaskerid@gmail.com>
2023-11-26 16:42:18 +0100

    initial

---
+++ arrays.php
@@ -0,0 +1,23 @@
+<?php
+//array
+$guitars = ['Vela','Explorer','Strat'];
+
+//assosiative arrasy
+$guitars2 = [
+ 'prs' => 'Vela',
+ 'gibson' => 'Explorer',
+ 'fender' => 'Strat',
+ 'Prs' => 'Starla'
+];
+
+// print_r($guitars);
+
+
+// if( isset($guitars[5]) ){
+//     echo $guitars[5];
+// }else{
+//     echo 'Guitar does not exist';
+// }
+
+
+// echo($guitars2['prs']);
---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>PHP tutorial</h1>
+    <div>
+        <?php 
+            include('conditions.php');
+            
+        
+            
+
+            
+            
+ 
+          
+        ?>
+
+    </div>
+    <div>
+        <?php 
+            include('conditions.php');
+            
+        
+            
+
+            
+            
+
+         
+        ?>
+
+    </div>
+    <h2>
+        Arrays
+    </h2>
+    <div>
+        <?php
+            include('arrays.php');
+
+        ?>
+    </div>
+    <h2>Loops</h2>
+    <div>
+        <a href="loops.php">loops</a>
+        
+    </div>
+
+    
+</body>
+</html>
